08a85957bfd7a913b52175e7b38874cde3f23b2e
Claire <c.riquier@gmail.com>
2015-03-19 14:25:17 +0100

    exo tableaus rss

--- 06_php/02_rss/index.php
+++ 06_php/02_rss/index.php
@@ -1,12 +1,19 @@
 <?php
 
-$monTableau = array();
+$article = array();;
+$article["title"] = "use airplay";;
+$article["description"] = "With AirPlay you can stream music";;
+$article["link"] = "support.apple.com/en-us/HT1222";;
+$article["guid"] = "http://support.apple.com/en-us/HT1222";;
 
-var_dump($monTableau);
-echo "<br/>";
+$article2 = array(
+    "title" => "If you can't send or receive messages on your iPhone",
+    "description" => "You might see an exclamation mark next to a message you try to send",
+    "link" => "support.apple.com/en-us/HT204065",
+    "guid" => "http://support.apple.com/en-us/HT204065"
+    );;
 
-$course = array("fruit", "orange","banane","pomme");
-    var_dump($course);
+var_dump($article);
+var_dump($article2)
 
-$course[viande] = ()
 ?>
